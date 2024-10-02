cc67f02bc2741322df15680a2cb889e247d61535
petukhov <petukhovg@gmail.com>
2024-10-02 20:14:43 +0800

    update

--- tiger_lexer/README.md
+++ tiger_lexer/README.md
@@ -2,4 +2,4 @@ Lexer for Tiger
 
 To run:
 - Go to the parent folder
-- opam exec -- dune exec json_lexer -w 
+- opam exec -- dune exec tiger_lexer ./tiger_lexer/simple.tig
--- tiger_lexer/ast.ml
+++
@@ -1,6 +0,0 @@
-type xml =
-  | Tag of string * xml list
-  | String of string 
-  | EMPTY 
-
-let example = Tag ("tag1", [ String "a"; Tag ("tag2", [ String "a"; String "b"; String "c" ]); String "c" ])
